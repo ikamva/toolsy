db26f01cacbbfd157912d68c3d46ae0d7d325ec5
Sivuyile Magutywa <s.magutywa@kernet.co.za>
2020-10-22 17:59:37 +0200

    added tool-card

---
+++ libs/tool-ui/src/lib/components/tool-card/tool-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tly-tool-card',
+  templateUrl: './tool-card.component.html',
+  styleUrls: ['./tool-card.component.scss']
+})
+export class ToolCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- libs/tool-ui/src/lib/tool-ui.module.ts
+++ libs/tool-ui/src/lib/tool-ui.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ToolCardComponent } from './components/tool-card/tool-card.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ToolCardComponent],
+  exports: [ToolCardComponent],
 })
 export class ToolUiModule {}
