d3898e9d2dc92ca4e2b7e170a527224a98ed3070
Sivuyile Magutywa <s.magutywa@kernet.co.za>
2021-02-04 11:28:15 +0200

    fixed tags

--- apps/toolsy/src/app/app.component.ts
+++ apps/toolsy/src/app/app.component.ts
@@ -4,29 +4,53 @@ import { ToolsyOverlayService } from '@toolsy/tool-overlay';
 import { Observable } from 'rxjs';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { OnInit } from '@angular/core';
+import { first, map } from 'rxjs/operators';
 
 @Component({
   selector: 'toolsy-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   tool: ITool;
   tools$: Observable<ITool[]>;
   categories$: Observable<ICategory[]>;
   categoryForm: FormGroup;
+  Categories: ICategory[];
 
   constructor(private toolsyOverlay: ToolsyOverlayService, private firestore: AngularFirestore, private fb: FormBuilder) {
-    this.tools$ = firestore.collection<ITool>('tools').valueChanges();
-    this.categories$ = firestore.collection<ICategory>('categories').valueChanges();
-    this.categoryForm = fb.group({
+
+  }
+
+  ngOnInit() {
+    this.tools$ = this.firestore.collection<ITool>('tools').valueChanges();
+
+    // Gets Categories
+    this.getCategories();
+
+    this.categoryForm = this.fb.group({
       category: [[]]
     })
   }
 
   get category() {
     return this.categoryForm.get('category')
+  }
+
+  async getCategories() {
+    this.Categories = await this.firestore.collection<ICategory>('categories').snapshotChanges().pipe(map(actions =>
+      actions.map(a => {
+        const data = a.payload.doc.data();
+        data.id = a.payload.doc.id;
+        return data;
+      })
+    ), first()).toPromise();
+  }
+
+  getTags(id: string) {
+    return this.Categories.find(category => category.id === id)
   }
 
   open(content: TemplateRef<any>) {
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
       colors: {
         backdrop: '#0C121B9C',
         dialog: '#F2F2F2',
+        tag: '#eff3f6'
       },
     },
   },
