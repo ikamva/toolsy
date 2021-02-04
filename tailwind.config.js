ac0d97e6d203e10ec42f0c5059e39b2577b68d27
Sivuyile Magutywa <s.magutywa@kernet.co.za>
2021-02-04 15:19:14 +0200

    upgrade tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = (isProd) => ({
+  prefix: '',
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: {
+    enabled: isProd,
+    content: ['./apps/**/*.html', './libs/**/*.html', '**/*.ts'],
+  },
+  theme: {
+    extent: {
+      colors: {
+        backdrop: '#0C121B9C',
+        dialog: '#F2F2F2',
+        tag: '#eff3f6'
+      },
+    },
+  },
+  variants: {
+  },
+  plugins: [],
+});
